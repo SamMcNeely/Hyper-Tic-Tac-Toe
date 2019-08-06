d44d77ecd90370ef250a073b94d523a475161471
SamMcNeely <samuelemcneely@gmail.com>
2019-08-06 01:29:03 -0700

    Set Screen Resolution

--- Assets/_Scripts/MainMenuController.cs
+++ Assets/_Scripts/MainMenuController.cs
@@ -8,7 +8,7 @@ public class MainMenuController : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        Screen.SetResolution(600, 800, false);
     }
 
     // Update is called once per frame
